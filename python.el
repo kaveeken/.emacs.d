0a19c9ac002e3a58e875afa6ef973b818351271e
Kris <k.a.veeken@student.rug.nl>
2021-09-07 14:48:06 +0200

    replaced lsp and flycheck with elpy

--- python.el
+++ python.el
@@ -3,34 +3,21 @@
 :defer t
 :diminish
 :config
-
-	(setenv "WORKON_HOME" "/home/kris/.pyenv/versions/")
+(setenv "WORKON_HOME" "/home/kris/.pyenv/versions/")
 	; Show python venv name in modeline
-	(setq pyvenv-mode-line-indicator '(pyvenv-virtual-env-name ("[venv:" pyvenv-virtual-env-name "] ")))
-	(pyvenv-mode t))
-(use-package lsp-mode
-    :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-            (python-mode . lsp))
-            ;; if you want which-key integration
-            ;; (lsp-mode . lsp-enable-which-key-integration))
-    :commands lsp)
+(setq pyvenv-mode-line-indicator '(pyvenv-virtual-env-name ("[venv:" pyvenv-virtual-env-name "] ")))
+(pyvenv-mode t))
 
-(use-package lsp-jedi
+(use-package elpy
   :ensure t
-  :config
-  (with-eval-after-load "lsp-mode"
-    (add-to-list 'lsp-disabled-clients 'pyls)
-    (add-to-list 'lsp-enabled-clients 'jedi)))
+  :init
+  (elpy-enable))
 
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
+(defvar python-default-pyenv "tweez-3.8.6")
 
 (add-hook 'python-mode-hook
 	  (lambda ()
-	    (pyvenv-workon "tweez-3.8.6")
-	    ;(make-local-variable 'flycheck-checker)
-	    ;(setq-local flycheck-checker 'python-flake8)
+	    (pyvenv-workon python-default-pyenv)
 	    (make-local-variable 'display-fill-column-indicator-column)
 	    (setq-local display-fill-column-indicator-column 80)
 	    (display-fill-column-indicator-mode)))
