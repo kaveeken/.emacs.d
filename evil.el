86ef3758d9a8a32677e7ca7cd97b8e82600fdb18
Kris <k.a.veeken@student.rug.nl>
2021-09-07 17:32:08 +0200

    Resolved conflicting keybindings for company-mode selection menu

--- evil.el
+++ evil.el
@@ -4,3 +4,7 @@
 (require 'evil-snipe)
 (evil-snipe-mode +1)
 (evil-snipe-override-mode +1)
+
+;; these interfere with my company-mode binds
+(define-key evil-insert-state-map (kbd "C-n") nil)
+(define-key evil-insert-state-map (kbd "C-p") nil)
