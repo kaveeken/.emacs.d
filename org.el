ad80b69982d48ee46d8dfaacdd43da91ce6cba2e
Kris <k.a.veeken@student.rug.nl>
2021-09-08 14:40:04 +0200

    minted for latex export code blocks (requires Pygments library)

--- org.el
+++ org.el
@@ -1,6 +1,12 @@
 (setq org-default-notes-file "~/org/notes.org")
 (setq org-adapt-indentation nil)
 
+(setq org-latex-listings 'minted
+      org-latex-packages-alist '(("" "minted"))
+      org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
