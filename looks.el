42d06e78bebfa30df10542e2f824ed889a488cc8
Kris <k.a.veeken@student.rug.nl>
2021-09-08 13:04:56 +0200

    ensured doom-themes

--- looks.el
+++ looks.el
@@ -2,7 +2,19 @@
   :ensure t
   :init (doom-modeline-mode 1))
 
-(load-theme 'doom-one t)
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
 (defun synchronize-theme ()
   (setq hour
     (string-to-number
