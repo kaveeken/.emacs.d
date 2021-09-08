6d3bdc92f6bd4d93381a572ce3a5b0c444ad4dc7
Kris <k.a.veeken@student.rug.nl>
2021-09-08 12:30:48 +0200

    evil, evil-snipe loaded through use-package

--- evil.el
+++ evil.el
@@ -1,10 +1,15 @@
-(require 'evil)
-(define-key evil-normal-state-map (kbd "C-b") 'evil-scroll-up)
-(evil-mode 1)
-(require 'evil-snipe)
-(evil-snipe-mode +1)
-(evil-snipe-override-mode +1)
+(use-package evil-snipe
+  :ensure t
+  :init
+  (add-hook 'evil-mode (evil-snipe-mode +1))
+  :config
+  (evil-snipe-override-mode +1))
 
-;; these interfere with my company-mode binds
-(define-key evil-insert-state-map (kbd "C-n") nil)
-(define-key evil-insert-state-map (kbd "C-p") nil)
+(use-package evil
+  :ensure t
+  :init (evil-mode 1)
+  :config
+  (define-key evil-insert-state-map (kbd "C-n") nil) ;; interfere with
+  (define-key evil-insert-state-map (kbd "C-p") nil) ;; company
+  (define-key evil-normal-state-map (kbd "C-b") 'evil-scroll-up))
+  
