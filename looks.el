0721d3d3e4fb8841a8403e4b42bf4a68e75a00de
Kris <k.a.veeken@student.rug.nl>
2021-09-08 12:58:28 +0200

    synchronize-theme works with error. f5 sets window width to 80c.

--- looks.el
+++ looks.el
@@ -5,13 +5,13 @@
 (load-theme 'doom-one t)
 (defun synchronize-theme ()
   (setq hour
-	(string-to-number
-	 (substring (current-time-string) 11 13)))
+    (string-to-number
+     (substring (current-time-string) 11 13)))
   (if (member hour (number-sequence 6 17))
       (load-theme 'doom-acario-light t) ;; redundant load-themes
     (load-theme 'doom-one t)))
 
-;(run-with-timer 0 3600 synchronize-theme)
+(run-with-timer 0 3600 (synchronize-theme))
 
 
 (global-display-line-numbers-mode 1)
@@ -32,3 +32,15 @@
 
 (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
 ;(setq initial-buffer-choice "*dashboard*")
+
+
+(defun set-window-width (n)
+  "Set the selected window's width. Only works for windows on the left."
+  (adjust-window-trailing-edge (selected-window) (- n (window-width)) t))
+
+(defun set-80-columns ()
+  "Set the selected window (on the left side of the frame) to 80 columns."
+  (interactive)
+  (set-window-width 87)) ;; including line numbers etc
+
+(global-set-key (kbd "<f5>") 'set-80-columns)
